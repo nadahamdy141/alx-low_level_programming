6098448f32eacac435b481dc97bcd8a85035817f
nada hamdy <nadhamdy141@gmail.com>
2023-08-15 12:11:35 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10-function that will print alphabet 10x
+ *
+ *Description: prints alphabets usin prototype
+ *
+ *Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char m;
+
+	for (i = 0 ; i < 10 ; i++)
+
+	{
+		for (m = 'a' ; m <= 'z' ; m++
+				_putchar(m);
+	}
+	_putchar('\n');
+	}
