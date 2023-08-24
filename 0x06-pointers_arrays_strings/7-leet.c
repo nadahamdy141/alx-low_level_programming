ab5fc0fb5e8b4da1cd641187041157df5f756e9f
nada hamdy <nadhamdy141@gmail.com>
2023-08-24 05:13:28 -0700

    my first commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *leet - encodes a string into 1337.
- *letters a and A are replaced by 4
+ *letters a and A are replaced by 4.
  *Letters e and E are replaced by 3
  *Letters o and O are replaced by 0
  *Letters t and T are replaced by 7
