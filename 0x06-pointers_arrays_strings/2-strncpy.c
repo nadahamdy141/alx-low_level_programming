8f9859e3cc5e792d405bc8c834c6f7bba6c68cf7
nada hamdy <nadhamdy141@gmail.com>
2023-08-26 13:59:20 -0700

    my first commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,20 +5,20 @@
  *@dest: pointer to destination
  *@src: pointer to source
  *@n: number of bytes to be used
+ *
  *Return: to dest
  */
 char *_strncpy(char *dest, char *src, int n)
-
 {
-	int i;
+int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (;i < n i++);
-	{
-		dest[i] = '\0';
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (;i < n i++);
+{
+dest[i] = '\0';
 
-	}
+}
 
-	return (dest);
+return (dest);
 }
