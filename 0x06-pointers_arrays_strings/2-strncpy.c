ae6a7b6a31a1fbfa80e796ce9025414df5655722
nada hamdy <nadhamdy141@gmail.com>
2023-08-26 13:25:42 -0700

    my first commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strncpy - is a function that copies String
+ *@dest: pointer to destination
+ *@src: pointer to source
+ *@n: number of bytes to be used
+ *Return: to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0' i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
