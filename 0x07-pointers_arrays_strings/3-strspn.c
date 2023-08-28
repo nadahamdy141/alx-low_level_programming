654b7d3818f6f220b24411b7d26b6a85ca08491f
nada hamdy <nadhamdy141@gmail.com>
2023-08-28 14:50:10 -0700

    my first commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,28 @@
 #include "main.h"
-
 /**
- * _strspn - Entry point that return the lenght
- * @s: input string 
- * @accept: input string 
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
  * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	int j;
+	int i;
 
 	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accept[j])
+			if (*s == accept[i])
 			{
 				n++;
 				break;
 			}
-			else if (accept[j + 1] == '\0')
+			else if (accept[i + 1] == '\0')
 				return (n);
 		}
 		s++;
 	}
 	return (n);
+}
