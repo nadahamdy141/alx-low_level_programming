06259a156c3b50b1b61872954cfb6a9b1a7f57f3
nada hamdy <nadhamdy141@gmail.com>
2023-08-31 06:48:48 -0700

    my first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_puts_recursion-print a string,followed by new line
+*@s:string to print
+*Return : Always 0
+*/
+void _puts_recursion(char *s);
+
+{
+	if (*s == \'0')
+		putchar (\'n);
+	Return;
+	_*putchar (*s);
+	_puts_recursion
+}
