aba53fd7480e6d057a2df275ece5277d0e5cd383
nada hamdy <nadhamdy141@gmail.com>
2023-08-31 15:49:21 -0700

    my first commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
-#iclude "main.h"
+#include "main.h"
 /**
  *_strlen_recursion -  return the lenght of a string
- *@s: calculate the lenght of the string
+ * @s: calculate the lenght of the string
  *
- * return: the lenght of the string
+ * Return: the lenght of the string
  */
 
 int _strlen_recursion(char *s)
