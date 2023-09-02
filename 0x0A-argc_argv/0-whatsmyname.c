64ba4951f8c2e20a3b496564569f44e929df9799
nadahamdy <nadahamdy89@hotmail.com>
2023-09-02 05:39:33 +0300

    my first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
-#include "main.h"
-#include <stdio.h>
+#include "stdio.h"
 
 /**
- * main - this is a function to print its name
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: 0 for success
+ * main - function with two arguments
+ * @argc: int type argument
+ * @argv: char type pointer array argument
+ *
+ * Description: prints all arguments it recieves
+ * Return: void
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
