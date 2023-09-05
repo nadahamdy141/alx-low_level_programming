ccc9b9cd389f174d99017b5742632de209d53e26
nadahamdy <nadahamdy89@hotmail.com>
2023-09-05 22:35:34 +0300

    my first commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - function that concatenates two strings
+ * @grid: parmater for grid
+ * @height: parameter for height
+ * Return: Always 0.
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid)
+}
