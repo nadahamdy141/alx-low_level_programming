c9c70b8ebff178ab220c8bb72578d48f296f57b4
nada hamdy <nadhamdy141@gmail.com>
2023-09-26 14:37:35 +0300

    n

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *head)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (head)
-    {
-        printf("%d\n", head->n);
-        num++;
-        head = head->next;
-    }
+	while (head)
+	{
+		printf("%d\n", head->n);
+		num++;
+		head = head->next;
+	}
 
-    return (num);
+	return (num);
 }
