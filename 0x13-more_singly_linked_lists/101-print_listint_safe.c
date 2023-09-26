873c7298f2c863fee899fd902de495dd12ec26d0
nada hamdy <nadhamdy141@gmail.com>
2023-09-26 18:16:28 +0300

    n

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
@@ -7,7 +8,7 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t num = NULL;
+    size_t num = 0;
     long int diff;
 
     while (head)
